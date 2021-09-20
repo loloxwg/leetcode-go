2775d4f91ced57d436852c8486178cd58162ab2f
loloxwg <loloxwg@outlook.com>
2021-09-20 21:17:07 +0800

    :hankey: Writing bad code that needs to be improved.别的二分法

--- leetcode/9.20/num1.go
+++ leetcode/9.20/num1.go
@@ -11,3 +11,51 @@ func searchRange(nums []int, target int) []int {
 	rightmost := sort.SearchInts(nums, target+1) - 1
 	return []int{leftmost, rightmost}
 }
+
+func searchRange2(nums []int, target int) []int {
+	n := len(nums)
+	l := 0
+	r := n - 1
+	targetMid := -1
+	start := 0
+	end := 0
+
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			targetMid = mid
+			break
+		}
+		//target 在 mid-r之间, l = mid +1
+		if nums[mid] < target && target <= nums[r] {
+			l = mid + 1
+		} else {
+			//target 在 l - mid 之间，r = mid -1
+			r = mid - 1
+		}
+
+	}
+
+	if targetMid == -1 {
+		return []int{-1, -1}
+	} else {
+		//找target的上下边界
+		for i := targetMid; i >= 0; i-- {
+			if nums[i] == target {
+				start = i
+
+			} else {
+				break
+			}
+		}
+		for i := targetMid; i < n; i++ {
+			if nums[i] == target {
+				end = i
+
+			} else {
+				break
+			}
+		}
+		return []int{start, end}
+	}
+}
--- leetcode/9.20/num1_test.go
+++ leetcode/9.20/num1_test.go
@@ -15,7 +15,7 @@ func Test_searchRange(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+
 		{
 			"在排序数组中查找元素的第一个和最后一个位置",
 			args{
@@ -29,6 +29,34 @@ func Test_searchRange(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_searchRange2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"在排序数组中查找元素的第一个和最后一个位置",
+			args{
+				[]int{5, 7, 7, 8, 8, 10},
+				8,
+			},
+			[]int{3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
